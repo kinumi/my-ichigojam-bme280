622e138fe6c1f8934b8d1c304e894b9010c39274
kinumi <177430+kinumi@users.noreply.github.com>
2020-01-21 00:03:07 +0900

    調整

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-export FLASK_APP=/home/kunimi_ikeda/my-ichigojam-bme280/app.py
+export FLASK_APP=/home/ubuntu/my-ichigojam-bme280/app.py
 export LANG=ja_JP.UTF-8
 /home/kunimi_ikeda/.pyenv/shims/flask run --debugger --reload -p 80 -h 0.0.0.0
